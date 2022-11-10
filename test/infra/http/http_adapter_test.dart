01f8f6fd386113d5b29c1567d6350f6bac37a7ee
Iam-luik <castro.luik@hotmail.com>
2022-11-09 21:37:10 -0300

    ensure call post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
@@ -8,12 +10,16 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void>? request({required String url, required String method}) async {
+  Future<void>? request(
+      {required String url, required String method, Map? body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(Uri.parse(url), headers: headers);
+
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
   }
 }
 
@@ -41,12 +47,24 @@ void main() {
         'accept': 'application/json'
       };
 
-      when(() => client.post(Uri.parse(url), headers: headers))
+      final body = {'any_key': 'any_value'};
+
+      when(() => client.post(Uri.parse(url), headers: headers, body: body))
+          .thenAnswer((_) async => Response('{}', 200));
+
+      await sut.request(url: url, method: 'post', body: body);
+
+      verify(() => client.post(Uri.parse(url),
+          headers: headers, body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      when(() => client.post(any(), headers: any(named: 'headers')))
           .thenAnswer((_) async => Response('{}', 200));
 
       await sut.request(url: url, method: 'post');
 
-      verify(() => client.post(Uri.parse(url), headers: headers));
+      verify(() => client.post(Uri.parse(url), headers: any(named: 'headers')));
     });
   });
 }
